43f4dad7adff7eb930f2783ba60f06713e8a46ab
uturkarslan <ugurturkarslan@gmail.com>
2022-09-29 21:54:36 +0200

    Simulate multiple tables

--- mysql/simulator/main.go
+++ mysql/simulator/main.go
@@ -4,12 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var DB_DRIVER string = "mysql"
+var NUM_TABLES int = 5
 
 func main() {
 
@@ -18,19 +20,65 @@ func main() {
 	username := "myuser"
 	password := os.Getenv("PASSWORD")
 	dbName := "mydb"
-	tableName := "mytable"
 
+	db := connectToMysql(username, password, ipAddress)
+	defer db.Close()
+
+	createDatabase(db, dbName)
+
+	for i := 1; i < NUM_TABLES; i++ {
+		tableName := "mytable" + strconv.Itoa(i)
+		createTable(db, tableName)
+	}
+
+	for {
+
+		for i := 1; i < NUM_TABLES; i++ {
+			tableName := "mytable" + strconv.Itoa(i)
+
+			if i == 0 { // For table 1
+
+				// Insert 2 value every loop
+				for i := 1; i < 2; i++ {
+					insert(db, tableName)
+				}
+			} else if i == 1 { // For table 2
+
+				// Insert 3 value every loop
+				for i := 1; i < 3; i++ {
+					insert(db, tableName)
+				}
+			} else { // For the rest of the tables
+
+				// Insert 1 value every loop
+				insert(db, tableName)
+			}
+		}
+	}
+}
+
+func connectToMysql(
+	username string,
+	password string,
+	ipAddress string,
+) *sql.DB {
 	conn := username + ":" + password + "@tcp(" + ipAddress + ":3306)/"
 	fmt.Println("Connecting to MySQL [" + conn + "]...")
 	db, err := sql.Open(DB_DRIVER, conn)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
 	fmt.Println(" -> Connected to MySQL.")
 
+	return db
+}
+
+func createDatabase(
+	db *sql.DB,
+	dbName string,
+) {
 	fmt.Println("Creating DB...")
-	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS " + dbName)
+	_, err := db.Exec("CREATE DATABASE IF NOT EXISTS " + dbName)
 	if err != nil {
 		panic(err)
 	}
@@ -42,25 +90,31 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(" -> DB on use.")
+}
 
+func createTable(
+	db *sql.DB,
+	tableName string,
+) {
 	fmt.Println("Creating table...")
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS " + tableName + "( id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, data VARCHAR(255) )")
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS " + tableName + "( id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, data VARCHAR(255) )")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(" -> Created table.")
+}
 
-	for {
-		func() {
-			fmt.Println("Inserting value...")
-			insert, err := db.Query("INSERT INTO " + tableName + "(data) VALUES ( 'TEST' )")
-			if err != nil {
-				panic(err.Error())
-			}
-			defer insert.Close()
-			fmt.Println(" -> Inserted value.")
-
-			time.Sleep(2 * time.Second)
-		}()
+func insert(
+	db *sql.DB,
+	tableName string,
+) {
+	fmt.Println("Inserting value...")
+	insert, err := db.Query("INSERT INTO " + tableName + "(data) VALUES ( 'TEST' )")
+	if err != nil {
+		panic(err.Error())
 	}
+	defer insert.Close()
+	fmt.Println(" -> Inserted value.")
+
+	time.Sleep(2 * time.Second)
 }
